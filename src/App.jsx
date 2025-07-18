f12b5997a6dbcd96eb1ecfc96e7cb1c5c2d49e2c
annakadiake <168558430+annakadiake@users.noreply.github.com>
2025-07-18 19:59:40 +0000

    Fix Navigate import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthProvider'
 import { Toaster } from 'react-hot-toast'
 import Login from './pages/auth/Login'
